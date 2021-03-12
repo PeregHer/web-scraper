64991fe54e1be890ddde9de26fbc29359af0f59b
PeregHer <pereg.hergoualch@isen-ouest.yncrea.fr>
2021-03-12 20:42:58 +0100

    main commit

---
+++ saver/saver.py
@@ -0,0 +1,45 @@
+import os
+import pymongo
+import json
+import time
+
+
+class Saver:
+    @classmethod
+    def find_files(cls):
+        for root, dirs, files in os.walk("data", topdown=True):
+            return files
+
+    @classmethod
+    def load_json(cls, path):
+        with open('data/' + path) as json_file:
+            return json.load(json_file)
+
+    @classmethod
+    def open_connexion(cls):
+        cls.client = pymongo.MongoClient("mongodb+srv://Stephane:isen-brest@bel-cluster.1cbyc.mongodb.net/coincap?retryWrites=true&w=majority")
+        cls.db = cls.client['coincap']
+        cls.collection = cls.db['data']
+    
+    @classmethod
+    def close_connexion(cls):
+        cls.client.close()
+
+    @classmethod
+    def insert(cls, data):
+        cls.open_connexion()
+        cls.collection.insert_one(data)
+        cls.close_connexion()
+
+    @classmethod
+    def insert_files(cls):
+        files = cls.find_files()
+        for file in files:
+            data = cls.load_json(file)
+            cls.insert(data)
+            os.remove('data/' + file)
+
+
+while True:
+    Saver.insert_files()
+    time.sleep(600)
---
+++ scrapper/scrapper.py
@@ -0,0 +1,46 @@
+import selenium
+from selenium import webdriver
+import time
+import datetime
+import json
+
+def multiply(number):
+    mult = number[-1]
+    value = float(number[:-1])
+    mults = {'t': 10**12, 'b': 10**9, 'm': 10**6, 'k': 10**3}
+    return int(value * mults[mult])
+
+class Scrapper:
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--headless")
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("--disable-dev-shm-usage")
+
+        self.browser = webdriver.Chrome(options=self.options)
+        self.browser.get('https://coincap.io/')
+
+        time.sleep(3)
+        button = self.browser.find_element_by_xpath("//*[contains(text(), 'View More')]")
+
+    def search(self):
+        stocks = {}
+        for i in range(1, 21):
+            name = self.browser.find_element_by_xpath(f'/html/body/div[2]/main/div[4]/div/table/tbody/tr[{i}]/td[2]/div/a').text.split('\n')
+            price = float(self.browser.find_element_by_xpath(f'/html/body/div[2]/main/div[4]/div/table/tbody/tr[{i}]/td[3]/span').text.replace('$', '').replace(',', ''))
+            cap = self.browser.find_element_by_xpath(f'/html/body/div[2]/main/div[4]/div/table/tbody/tr[{i}]/td[4]/span').text.replace('$', '')
+            volume = self.browser.find_element_by_xpath(f'/html/body/div[2]/main/div[4]/div/table/tbody/tr[{i}]/td[7]/span').text.replace('$', '')
+            stocks[name[1]] = {'name': name[0], 'price': price, 'cap': multiply(cap), 'volume': multiply(volume)}
+        self.data = {'time': datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"), 'data': stocks}
+    
+    def save_json(self):
+        with open(f'data/{datetime.datetime.now().timestamp()}.json', 'w') as fp:
+            json.dump(self.data, fp,  indent=4)
+
+
+scrapper = Scrapper()
+
+while True:
+    scrapper.search()
+    scrapper.save_json()
+    time.sleep(60)
